839a5a04773b99d0aa8e2a597f15c9c706d12abb
thangathirupathi1297 <64582222+thangathirupathi1297@users.noreply.github.com>
2022-04-20 18:34:34 +0530

    demo

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+  }
